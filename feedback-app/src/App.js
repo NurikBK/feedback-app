0daba8488cb5f2e4f4fc76796e270c76b7d75bb2
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-04 18:29:35 +0600

    updated

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 
 import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/feedbackData';
 
 function App() {
- 
+  const [feedback, setFeedback] = useState(FeedbackData);
 
   return (
     <>
-      <Header text="true" />
+      <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
