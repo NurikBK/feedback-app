fe391683a5b5a268290b260c5abb03215c180daa
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-04 17:41:53 +0600

    Update App.js

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,9 +1,9 @@
+
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
 
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my block post';
+ 
 
   return (
     <>
