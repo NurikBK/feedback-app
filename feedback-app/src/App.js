8740943e7a9b2c463948bdd5c3e694c172bafae6
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-05 14:03:15 +0600

    Update App.js

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -3,17 +3,21 @@ import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/feedbackData';
+import FeedbackStats from './components/FeedbackStats';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
   function handleDelete(id) {
-    setFeedback((prevData) => prevData.filter((item) => item.id != id));
+    if (window.confirm('Are you sure you want to delete')) {
+      setFeedback(feedback.filter((item) => item.id != id));
+    }
   }
 
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={handleDelete} />
       </div>
     </>
