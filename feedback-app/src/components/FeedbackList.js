571f1c8009102cd5a299842497c8cec603ad3799
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-05 17:18:29 +0600

    Update FeedbackList.js
    commented id proptypes

--- feedback-app/src/components/FeedbackList.js
+++ feedback-app/src/components/FeedbackList.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import FeedbackItem from './FeedbackItem';
 
 function FeedbackList({ feedback, handleDelete }) {
-
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
   }
@@ -20,7 +19,7 @@ function FeedbackList({ feedback, handleDelete }) {
 FeedbackList.propTypes = {
   feedback: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      // id: PropTypes.number.isRequired,
       rating: PropTypes.number.isRequired,
       text: PropTypes.string.isRequired,
     })
