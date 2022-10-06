4a1d1dd3b03e921c58d1074985d47cc3686ebcbf
Nurzhan Bizhan <bknurik@gmail.com>
2022-10-06 15:44:22 +0600

    Update App.js

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,11 +1,15 @@
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/feedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import About from './pages/About';
+import AboutLink from './components/AboutLink';
+
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -17,18 +21,31 @@ function App() {
 
   function addFeedback(newFeedback) {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback])
+    setFeedback([newFeedback, ...feedback]);
   }
 
   return (
-    <>
+    <FeedbackProvider>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={handleDelete} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={handleDelete} />
+                <AboutLink />
+              </>
+            }
+          />
+
+          <Route path="/about" element={<About />} />
+        </Routes>
       </div>
-    </>
+    </FeedbackProvider>
   );
 }
 
